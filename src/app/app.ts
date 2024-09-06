f125cde2f2cd5b03453a6cebd81862492ad7bd69
saurabhtiwari <saurabh.tiwari@sanchalansoft.com>
2024-09-06 08:47:09 +0530

    rollback app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -36,12 +36,11 @@ export class App implements OnInit {
     this.elementRef.nativeElement.removeAttribute("ng-server-context");
 
     if(environment.production){
-
-      // const script = this.renderer.createElement('script');
-      // script.defer = true;
-      // script.src = 'https://cloud.umami.is/script.js';
-      // script.setAttribute('data-website-id', environment.umamiWebsiteId);
-      // this.renderer.appendChild(this.document.head, script);
+      const script = this.renderer.createElement('script');
+      script.defer = true;
+      script.src = 'https://cloud.umami.is/script.js';
+      script.setAttribute('data-website-id', environment.umamiWebsiteId);
+      this.renderer.appendChild(this.document.head, script);
     }
   }
 
